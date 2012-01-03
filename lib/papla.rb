6763befb9fee00636338805b58b19cd99e4a3c91
Olek Janiszewski <olek.janiszewski@gmail.com>
2012-01-03 04:58:30 +0100

    Convert two-digit numbers

--- lib/papla.rb
+++ lib/papla.rb
@@ -3,14 +3,25 @@ require "papla/version"
 
 module Papla
   def self.convert(number)
-    WORDS[number].capitalize
+    if number < 20
+      ONES[number]
+    else
+      tens = number / 10
+      ones = number - tens * 10
+      [TENS[tens], ones.zero? ? nil : ONES[ones]].compact.join(' ')
+    end.capitalize
   end
 
   private
-  WORDS = %w[
+  ONES = %w[
     zero jeden dwa trzy cztery pięć
     sześć siedem osiem dziewięć dziesięć
     jedenaście dwanaście trzynaście czternaście piętnaście
-    szesnaście siedemnaście osiemnaście dziewiętnaście dwadzieścia
+    szesnaście siedemnaście osiemnaście dziewiętnaście
   ].freeze
+
+  TENS = %w[
+    nil nil dwadzieścia trzydzieści czterdzieści pięćdziesiąt
+    sześćdziesiąt siedemdziesiąt osiemdziesiąt dziewięćdziesiąt
+  ]
 end
