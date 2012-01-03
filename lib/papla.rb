75d984c02e22b8274b53aaeee2b8cff581d5f8a5
Olek Janiszewski <olek.janiszewski@gmail.com>
2012-01-03 06:01:41 +0100

    Convert three-digit numbers

--- lib/papla.rb
+++ lib/papla.rb
@@ -3,25 +3,44 @@ require "papla/version"
 
 module Papla
   def self.convert(number)
-    if number < 20
-      ONES[number]
+    if number.zero?
+      ZERO
     else
-      tens = number / 10
-      ones = number - tens * 10
-      [TENS[tens], ones.zero? ? nil : ONES[ones]].compact.join(' ')
+      split(number).join(' ')
     end.capitalize
   end
 
   private
-  ONES = %w[
-    zero jeden dwa trzy cztery pięć
+  ZERO = 'zero'
+
+  ONES = [nil] + %w[
+    jeden dwa trzy cztery pięć
     sześć siedem osiem dziewięć dziesięć
     jedenaście dwanaście trzynaście czternaście piętnaście
     szesnaście siedemnaście osiemnaście dziewiętnaście
   ].freeze
 
-  TENS = %w[
-    nil nil dwadzieścia trzydzieści czterdzieści pięćdziesiąt
+  TENS = [nil, nil] + %w[
+    dwadzieścia trzydzieści czterdzieści pięćdziesiąt
     sześćdziesiąt siedemdziesiąt osiemdziesiąt dziewięćdziesiąt
-  ]
+  ].freeze
+
+  HUNDREDS = [nil] + %w[
+    sto dwieście trzysta czterysta pięćset
+    sześćset siedemset osiemset dziewięćset
+  ].freeze
+
+  def self.split(number)
+    if number.zero?
+      nil
+    elsif number < 20
+      [ONES[number]]
+    elsif number < 100
+      tens, remainder = number.divmod(10)
+      [TENS[tens], *split(remainder)]
+    else
+      hundreds, remainder = number.divmod(100)
+      [HUNDREDS[hundreds], *split(remainder)]
+    end
+  end
 end
--- spec/papla_spec.rb
+++ spec/papla_spec.rb
@@ -45,4 +45,51 @@ describe Papla do
     subject[90].should eq('Dziewięćdziesiąt')
     subject[98].should eq('Dziewięćdziesiąt osiem')
   end
+
+  it 'joins number of hundreds, tens and ones' do
+    subject[100].should eq('Sto')
+    subject[101].should eq('Sto jeden')
+    subject[111].should eq('Sto jedenaście')
+    subject[122].should eq('Sto dwadzieścia dwa')
+
+    subject[200].should eq('Dwieście')
+    subject[202].should eq('Dwieście dwa')
+    subject[212].should eq('Dwieście dwanaście')
+    subject[233].should eq('Dwieście trzydzieści trzy')
+
+    subject[300].should eq('Trzysta')
+    subject[303].should eq('Trzysta trzy')
+    subject[313].should eq('Trzysta trzynaście')
+    subject[344].should eq('Trzysta czterdzieści cztery')
+
+    subject[400].should eq('Czterysta')
+    subject[404].should eq('Czterysta cztery')
+    subject[414].should eq('Czterysta czternaście')
+    subject[455].should eq('Czterysta pięćdziesiąt pięć')
+
+    subject[500].should eq('Pięćset')
+    subject[505].should eq('Pięćset pięć')
+    subject[515].should eq('Pięćset piętnaście')
+    subject[566].should eq('Pięćset sześćdziesiąt sześć')
+
+    subject[600].should eq('Sześćset')
+    subject[606].should eq('Sześćset sześć')
+    subject[616].should eq('Sześćset szesnaście')
+    subject[677].should eq('Sześćset siedemdziesiąt siedem')
+
+    subject[700].should eq('Siedemset')
+    subject[707].should eq('Siedemset siedem')
+    subject[717].should eq('Siedemset siedemnaście')
+    subject[788].should eq('Siedemset osiemdziesiąt osiem')
+
+    subject[800].should eq('Osiemset')
+    subject[808].should eq('Osiemset osiem')
+    subject[818].should eq('Osiemset osiemnaście')
+    subject[899].should eq('Osiemset dziewięćdziesiąt dziewięć')
+
+    subject[900].should eq('Dziewięćset')
+    subject[909].should eq('Dziewięćset dziewięć')
+    subject[919].should eq('Dziewięćset dziewiętnaście')
+    subject[990].should eq('Dziewięćset dziewięćdziesiąt')
+  end
 end
